757fc9820aef8d7189839300637d8e9f55495918
hardik16solanki <hardik16solanki@gmail.com>
2018-06-15 08:18:03 +0530

    adding starting project

---
+++ script.js
@@ -0,0 +1,14 @@
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var css = document.getElementById("css");
+var body = document.getElementById("gradient");
+
+css.textContent = body.style.background;
+
+function setGradient() {
+	body.style.background = "linear-gradient(to right,"+ color1.value + "," + color2.value +")";
+	css.textContent = body.style.background;
+}
+
+color1.addEventListener("input",setGradient);
+color2.addEventListener("input",setGradient);
